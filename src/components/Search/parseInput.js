91952f6e209d6516ede9f1ee7435af210a0f64a5
Reyes <rreyes4@mdc.edu>
2019-09-27 08:46:10 -0400

    restructured so multiple keys may be used for each entry

--- src/components/Search/parseInput.js
+++ src/components/Search/parseInput.js
@@ -4,40 +4,54 @@ export default function parseInput(rawInput) {
   const { commands } = config;
 
   const input = rawInput.toLowerCase();
-  const keys = commands.map(command => command.key).flat();
+  const keysList = commands.map(command => command.keys).flat();
+  const ipPattern = new RegExp(/^((2(?!5?[6-9])|1|(?!0\d))\d\d?\.?\b){4}$/g);
   const urlPattern = new RegExp(
     /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+$/gi
   );
 
   // begin conditionals for the parser
   // handle match to key in config
-  if (keys.includes(input)) {
-    console.log(commands.filter(v => v.key.includes(input)));
-    return;
+  if (keysList.includes(input)) {
+    return commands.find(v => v.keys.includes(input)).url;
   }
 
   // handle urls
-  if (input.match(urlPattern) || input.includes("localhost")) {
-    return input.startsWith("http") ? input : "http://" + input;
+  if (
+    input.match(urlPattern) ||
+    input.match(ipPattern) ||
+    input.includes("localhost")
+  ) {
+    if (
+      input.match(ipPattern) ||
+      input.includes(".local") ||
+      input.includes("localhost")
+    ) {
+      return input.startsWith("http") ? input : "http://" + input;
+    } else {
+      return input.startsWith("http") ? input : "https://" + input;
+    }
   }
 
   // handle search with a matched key
-  if (input.includes(":") && keys.includes(input.split(":")[0])) {
+  if (input.includes(":") && keysList.includes(input.split(":")[0])) {
     const key = input.split(":")[0];
     const query = rawInput.split(":")[1].trimStart();
 
-    if (commands.find(command => command.key.includes(key)).search) {
+    if (commands.find(command => command.keys.includes(key)).search) {
       return commands
-        .find(command => command.key === key)
+        .find(command => command.keys.includes(key))
         .search.replace("{}", query);
     }
   }
 
   // handle paths with a matched key
-  if (input.includes("/") && keys.includes(input.split("/")[0])) {
+  if (input.includes("/") && keysList.includes(input.split("/")[0])) {
     const key = input.split("/")[0];
     const path = input.split("/")[1];
-    return commands.find(command => command.key.includes(key)).url + "/" + path;
+    return (
+      commands.find(command => command.keys.includes(key)).url + "/" + path
+    );
   }
 
   // search google
--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 /*
- * The category, name, key, url, search and for each command.
- * If none of the specified keys are matched, the '*' key is used.
- * Commands without a category won't populate the links list
+  At minimum each entry requires `url` & `keys` properties
+  `url` must be a string
+  `keys` must be an array, even if it's a single entry
  */
 
 export const config = {
@@ -15,94 +15,94 @@ export const config = {
     // Server
     {
       name: "Pi-hole",
-      key: ["pi", "pihole"],
+      keys: ["pihole", "pi"],
       url: "http://pi.hole/admin"
     },
     {
       name: "Homebridge",
-      key: "homebridge",
+      keys: ["homebridge", "hb"],
       url: "http://docker.local:8181"
     },
     {
       name: "Plex",
-      key: "plex",
+      keys: ["plex"],
       url: "http://server.local:32400/web"
     },
     {
       name: "Portainer",
-      key: "port",
+      keys: ["portainer", "port"],
       url: "http://docker.local:9000"
     },
     {
       name: "Radarr",
-      key: "radarr",
+      keys: ["radarr"],
       url: "http://docker.local:7878"
     },
     {
       name: "Sonarr",
-      key: "sonarr",
+      keys: ["sonarr"],
       url: "http://docker.local:8989"
     },
     {
       name: "Transmission",
-      key: "bt",
+      keys: ["transmission", "bt"],
       url: "http://docker.local:9091"
     },
     {
       name: "Unifi Controller",
-      key: "unifi",
+      keys: ["unifi"],
       url: "http://docker.local:8080"
     },
     // Me
     {
       category: "Me",
       name: "About",
-      key: "about",
+      keys: ["about"],
       url: "https://about.rolyreyes.com"
     },
     {
       category: "Me",
       name: "Blog",
-      key: "blog",
+      keys: ["blog"],
       url: "https://rolyreyes.com"
     },
     {
       category: "Me",
       name: "Dotfiles",
-      key: "dot",
+      keys: ["dot"],
       url: "https://github.com/ylor/dotfiles"
     },
     {
       category: "Me",
       name: "Github",
-      key: "git",
+      keys: ["github", "git"],
       url: "https://github.com/ylor?tab=repositories",
       search: "https://github.com/search?q={}"
     },
     {
       category: "Me",
       name: "Start",
-      key: "start",
+      keys: ["start"],
       url: "https://start.rolyreyes.com"
     },
     //Work
     {
       category: "MDC",
-      name: "MDC",
-      key: "mdc",
+      name: "Miami Dade College",
+      keys: ["mdc"],
       url: "http://www.mdc.edu"
     },
     {
       category: "MDC",
       name: "MDC - Employee",
-      key: "emp",
+      keys: ["emp"],
       url: "https://wapi.mdc.edu/NTAuth/account_admin.asp",
       search: "https://wapi.mdc.edu/NTAuth/user_data.asp?UserID={}"
     },
     {
       category: "MDC",
       name: "MDC - Student",
-      key: "stu",
+      keys: ["stu"],
       url: "https://mdcwapi.mdc.edu:8001/ntauthstudent",
       search:
         "https://mdcwapi.mdc.edu:8001/ntauthstudent/StudentData.aspx?AcctNm={}"
@@ -110,185 +110,185 @@ export const config = {
     {
       category: "MDC",
       name: "JAMF",
-      key: "jamf",
+      keys: ["jamf"],
       url: "https://kmacdep.kendall.mdcc.edu:8443"
     },
     {
       category: "MDC",
       name: "Timeclock",
-      key: ["tc", "timeclock"],
+      keys: ["tc", "timeclock"],
       url: "https://timeclock.mdc.edu"
     },
     // Internet
     {
       name: "1Password Password Generator",
-      key: "pw",
+      keys: ["pw"],
       url: "https://1password.com/password-generator"
     },
     {
       name: "Amazon",
-      key: "a",
+      keys: ["amazon", "a"],
       url: "https://www.amazon.com",
       search: "https://www.amazon.com/s?k={}"
     },
     {
       name: "Bitwarden",
-      key: "bw",
+      keys: ["bitwarden", "bw"],
       url: "https://vault.bitwarden.com"
     },
     {
       name: "Discord",
-      key: "discord",
+      keys: ["discord"],
       url: "https://discordapp.com/activity"
     },
     {
       name: "Drive",
-      key: "drive",
+      keys: ["google drive", "drive"],
       url: "https://drive.google.com"
     },
     {
       name: "Epic Game Store",
-      key: "egs",
+      keys: ["epic game store", "egs"],
       url: "https://www.epicgames.com/store",
       search: "https://www.epicgames.com/store/store-search/?q={}"
     },
     {
       name: "Giant Bomb",
-      key: "gb",
+      keys: ["giant bomb", "gb"],
       url: "https://www.giantbomb.com",
       search: "https://www.giantbomb.com/search/?i=&q={}"
     },
     {
       name: "Giant Bomb Infinite",
-      key: "gb8",
+      keys: ["giant bomb infinite", "gb8"],
       url: "https://www.giantbomb.com/infinite"
     },
     {
       name: "Giphy",
-      key: "gif",
+      keys: ["giphy", "gif"],
       url: "https://giphy.com",
       search: "https://giphy.com/search/{}"
     },
     {
       name: "Hacker News",
-      key: "hn",
+      keys: ["hacker news", "hn"],
       url: "https://news.ycombinator.com",
       search: "https://hn.algolia.com/?&query={}"
     },
     {
       name: "How Long to Beat",
-      key: "hltb",
+      keys: ["how lont to beat", "hltb"],
       url: "https://howlongtobeat.com",
       search: "https://howlongtobeat.com/?q={}"
     },
     {
       name: "iCloud",
-      key: "icloud",
+      keys: ["icloud"],
       url: "https://www.icloud.com"
     },
     {
       name: "iCloud Mail",
-      key: "mail",
+      keys: ["mail"],
       url: "https://www.icloud.com/#mail"
     },
     {
       name: "JSFiddle",
-      key: "fiddle",
+      keys: ["jsfiddle", "fiddle"],
       url: "https://jsfiddle.net"
     },
     {
       name: "MyAnimeList",
-      key: "mal",
+      keys: ["myanimelist", "mal"],
       url: "https://myanimelist.net",
       search: "https://myanimelist.net/search/all?q={}"
     },
     {
       name: "Netflix",
-      key: "netflix",
+      keys: ["netflix"],
       url: "https://www.netflix.com",
       search: "https://www.netflix.com/search?q={}"
     },
     {
       name: "Node Package Manager",
-      key: "npm",
+      keys: ["npm"],
       url: "https://www.npmjs.com",
       search: "https://www.npmjs.com/search?q={}"
     },
     {
       name: "Nyaa",
-      key: "nyaa",
+      keys: ["nyaa"],
       url: "https://nyaa.si",
       search: "https://nyaa.si/?f=0&c=0_0&q={}"
     },
     {
       name: "OneDrive",
-      key: "od",
+      keys: ["onedrive", "od"],
       url: "https://onedrive.live.com"
     },
     {
       name: "Plex.tv",
-      key: "plextv",
+      keys: ["plextv"],
       url: "https://app.plex.tv/desktop"
     },
     {
       name: "RARBG",
-      key: "rarbg",
+      keys: ["rarbg"],
       url: "https://rarbg.to/torrents",
       search: "https://rarbg.to/torrents?search={}"
     },
     {
       name: "Reddit",
-      key: "r",
+      keys: ["reddit", "r"],
       url: "https://www.reddit.com/r",
       search: "https://www.reddit.com/r/search?q={}"
     },
     {
       name: "Simplenote",
-      key: "sn",
+      keys: ["simplenote", "sn"],
       url: "https://app.simplenote.com"
     },
     {
       name: "Spotify",
-      key: "spotify",
+      keys: ["spotify"],
       url: "https://open.spotify.com"
     },
     {
       name: "ResetERA",
-      key: "re",
+      keys: ["resetera", "reset", "era", "re"],
       url: "https://www.resetera.com"
     },
 
     {
       name: "Steam",
-      key: "steam",
+      keys: ["steam"],
       url: "https://store.steampowered.com",
       search: "https://store.steampowered.com/search/?term={}"
     },
     {
       name: "Tumblr",
-      key: "tumblr",
+      keys: ["tumblr"],
       url: "https://tumblr.com"
     },
     {
       name: "Twitch",
-      key: "twitch",
+      keys: ["twitch"],
       url: "https://twitch.tv/directory"
     },
     {
       name: "Twitter",
-      key: "twitter",
+      keys: ["twitter"],
       url: "https://twitter.com",
       search: "https://twitter.com/search?q={}"
     },
     {
       name: "Yarn",
-      key: "yarn",
+      keys: ["yarn"],
       url: "https://yarnpkg.com",
       search: "https://yarnpkg.com//en/packages?q={}"
     },
     {
       name: "YouTube",
-      key: "yt",
+      keys: ["youtube", "yt"],
       url: "https://youtube.com",
       search: "https://youtube.com/results?search_query={}"
     },
@@ -296,26 +296,26 @@ export const config = {
     {
       category: "Destiny",
       name: "Destiny Item Manager",
-      key: "dim",
+      keys: ["destiny item manager", "dim"],
       url: "https://app.destinyitemmanager.com"
     },
     {
       category: "Destiny",
       name: "Destiny Sets",
-      key: "sets",
+      keys: ["destiny sets", "sets"],
       url: "https://destinysets.com"
     },
     {
       category: "Destiny",
       name: "Light.gg",
-      key: "light",
+      keys: ["lightgg", "light"],
       url: "https://light.gg"
     },
     // Reddit
     {
       category: "Reddit",
       name: "/r/DestinyTheGame",
-      key: "dtg",
+      keys: ["r/destinythegame", "r/dtg", "dtg"],
       url: "https://www.reddit.com/r/destinythegame",
       search:
         "https://www.reddit.com/r/destinythegame/search?q={}&restrict_sr=1"
@@ -323,24 +323,24 @@ export const config = {
     {
       category: "Reddit",
       name: "/r/HipHopHeads",
-      key: "r/hhh",
+      keys: ["r/hiphopheads", "r/hhh"],
       url: "https://www.reddit.com/r/hiphopheads",
       search: "https://www.reddit.com/r/hiphopheads/search?q={}&restrict_sr=1"
     },
     // Ugh
     {
       name: "/g/ - Technology",
-      key: "g",
+      keys: ["g"],
       url: "https://boards.4chan.org/g/catalog"
     },
     {
       name: "/w/ - Wallpapers",
-      key: "w",
+      keys: ["w"],
       url: "https://boards.4chan.org/w/catalog"
     },
     {
       name: "/wg/ - Wallpapers General",
-      key: "g",
+      keys: ["g"],
       url: "https://boards.4chan.org/wg/catalog"
     }
   ]
